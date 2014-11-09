ea83f5ae06bce2f75be1105d081f36c60d91062b
Matt Farmer <matt@frmr.me>
2014-11-09 15:53:24 -0500

    Implement Subscription meta object.

--- src/main/scala/me/frmr/stripe/Subscription.scala
+++ src/main/scala/me/frmr/stripe/Subscription.scala
@@ -1,8 +1,11 @@
 package me.frmr.stripe
 
+import net.liftweb.common._
 import net.liftweb.json._
   import JsonDSL._
 import net.liftweb.util.Helpers._
+
+import dispatch._
 
 case class Subscription(
   id: Option[String],
@@ -21,3 +24,55 @@ case class Subscription(
   discount: Option[Discount],
   raw: Option[JValue]
 ) extends StripeObject
+
+object Subscription extends ChildListable[SubscriptionList] with ChildGettable[Subscription] {
+  def baseResourceCalculator(req: Req, parentId: String) =
+    req / "customers" / parentId / "subscriptions"
+
+  def create(
+    customerId: String,
+    plan: String,
+    coupon: Option[String] = None,
+    trialEnd: Option[Long] = None,
+    card: Option[String] = None,
+    quantity: Option[Int] = None,
+    applicationFeePercent: Option[Double] = None,
+    metadata: Map[String, String] = Map.empty
+  )(implicit exec: StripeExecutor): Future[Box[Subscription]] = {
+    val params = List(
+      Some(("plan", plan)),
+      coupon.map(("coupon", _)),
+      trialEnd.map(trialEnd => ("trialEnd", trialEnd.toString)),
+      card.map(("card", _)),
+      quantity.map(quantity => ("quantity", quantity.toString)),
+      applicationFeePercent.map(fee => ("application_fee_percent", fee.toString))
+    ).flatten.toMap ++ metadataProcessor(metadata)
+
+    exec.executeFor[Subscription](baseResourceCalculator(exec.baseReq, customerId) << params)
+  }
+
+  def update(
+    customerId: String,
+    subscriptionId: String,
+    prorate: Option[Boolean] = None,
+    plan: Option[String] = None,
+    coupon: Option[String] = None,
+    trialEnd: Option[Long] = None,
+    card: Option[String] = None,
+    quantity: Option[Int] = None,
+    applicationFeePercent: Option[Double] = None,
+    metadata: Map[String, String] = Map.empty
+  )(implicit exec: StripeExecutor): Future[Box[Subscription]] = {
+    val params = List(
+      prorate.map(prorate => ("prorate", prorate.toString)),
+      plan.map(("plan", _)),
+      coupon.map(("coupon", _)),
+      trialEnd.map(trialEnd => ("trialEnd", trialEnd.toString)),
+      card.map(("card", _)),
+      quantity.map(quantity => ("quantity", quantity.toString)),
+      applicationFeePercent.map(fee => ("application_fee_percent", fee.toString))
+    ).flatten.toMap ++ metadataProcessor(metadata)
+
+    exec.executeFor[Subscription](baseResourceCalculator(exec.baseReq, customerId) / subscriptionId << params)
+  }
+}

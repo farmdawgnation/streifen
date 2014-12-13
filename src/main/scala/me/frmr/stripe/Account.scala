9efd926719be1ad26bffb3afb6d6c576e07a84d9
Matt Farmer <matt@frmr.me>
2014-12-13 11:41:44 -0500

    Implement Account object and meta object.

--- src/main/scala/me/frmr/stripe/Account.scala
+++ src/main/scala/me/frmr/stripe/Account.scala
@@ -1,7 +1,28 @@
 package me.frmr.stripe
 
+import net.liftweb.common._
 import net.liftweb.json._
   import JsonDSL._
+  import Extraction._
 import net.liftweb.util.Helpers._
 
-class Account(underlyingData: JValue)
+import dispatch._, Defaults._
+
+case class Account(
+  id: String,
+  chargesEnabled: Boolean,
+  country: String,
+  currenciesSupported: List[String],
+  defaultCurrency: String,
+  detailsSubmitted: Boolean,
+  transfersEnabled: Boolean,
+  displayName: String,
+  statementDescriptor: Option[String],
+  timezone: String,
+  raw: Option[JValue] = None
+) extends StripeObject
+
+object Account {
+  def get(implicit exec: StripeExecutor) =
+    exec.executeFor[Account](exec.baseReq / "account")
+}

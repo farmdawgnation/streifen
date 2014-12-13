f8a8a856e4ee290d1f7bff50177c30f75218e6bb
Matt Farmer <matt@frmr.me>
2014-12-13 11:33:57 -0500

    Implement meta object for tokens.

--- src/main/scala/me/frmr/stripe/Token.scala
+++ src/main/scala/me/frmr/stripe/Token.scala
@@ -1,8 +1,12 @@
 package me.frmr.stripe
 
+import net.liftweb.common._
 import net.liftweb.json._
   import JsonDSL._
+  import Extraction._
 import net.liftweb.util.Helpers._
+
+import dispatch._, Defaults._
 
 /**
  * Implementation of the Token structure in Stripe's API.
@@ -17,3 +21,54 @@ case class Token(
   card: Option[Card] = None,
   raw: Option[JValue] = None
 ) extends StripeObject
+
+object Token extends Gettable[Token] {
+  def baseResourceCalculator(req: Req) = req / "tokens"
+
+  def createForBankAccount(
+    country: String,
+    routingNumber: String,
+    accountNumber: String
+  )(implicit exec: StripeExecutor) = {
+    exec.executeFor[Token](baseResourceCalculator(exec.baseReq) << Map(
+      "bank_account[country]" -> country,
+      "bank_account[routing_number]" -> routingNumber,
+      "bank_account[account_number]" -> accountNumber
+    ))
+  }
+
+  def createForCard(
+    number: String,
+    expMonth: Int,
+    expYear: Int,
+    cvc: String,
+    name: Option[String] = None,
+    addressLine1: Option[String] = None,
+    addressLine2: Option[String] = None,
+    addressCity: Option[String] = None,
+    addressZip: Option[String] = None,
+    addressState: Option[String] = None,
+    addressCountry: Option[String] = None
+  )(implicit exec: StripeExecutor) = {
+    val requiredParams = Map(
+      "card[number]" -> number,
+      "card[exp_month]" -> expMonth.toString,
+      "card[exp_year]" -> expYear.toString,
+      "card[cvc]" -> cvc
+    )
+
+    val optionalParams = List(
+      name.map(("card[name]", _)),
+      addressLine1.map(("card[address_line1]", _)),
+      addressLine2.map(("card[address_line2]", _)),
+      addressCity.map(("card[address_city]", _)),
+      addressState.map(("card[address_state]", _)),
+      addressZip.map(("card[address_zip]", _)),
+      addressCountry.map(("card[address_country]", _))
+    ).flatten.toMap
+
+    val params = requiredParams ++ optionalParams
+
+    exec.executeFor[Token](baseResourceCalculator(exec.baseReq) << params)
+  }
+}

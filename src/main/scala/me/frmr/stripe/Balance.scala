5d8b055cfa34a6171acac3a0050713391d0e7a4c
Matt Farmer <matt@frmr.me>
2014-12-13 11:37:46 -0500

    Implement Balance object and meta object.

--- src/main/scala/me/frmr/stripe/Balance.scala
+++ src/main/scala/me/frmr/stripe/Balance.scala
@@ -1,7 +1,22 @@
 package me.frmr.stripe
 
+import net.liftweb.common._
 import net.liftweb.json._
   import JsonDSL._
+  import Extraction._
 import net.liftweb.util.Helpers._
 
-class Balance(underlyingData: JValue)
+import dispatch._, Defaults._
+
+case class BalanceItem(amount: Long, currency: String)
+case class Balance(
+  livemode: Boolean,
+  pending: List[BalanceItem],
+  available: List[BalanceItem],
+  raw: Option[JValue] = None
+) extends StripeObject
+
+object Balance {
+  def get(implicit exec: StripeExecutor) =
+    exec.executeFor[Balance](exec.baseReq / "balance")
+}

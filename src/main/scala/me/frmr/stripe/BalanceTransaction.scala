47d57a47a2f96cee62937eebb9bba02fcf04efbb
Matt Farmer <matt@frmr.me>
2014-12-14 15:03:33 -0500

    Make BalanceTransaction a thing.

---
+++ src/main/scala/me/frmr/stripe/BalanceTransaction.scala
@@ -0,0 +1,37 @@
+package me.frmr.stripe
+
+import net.liftweb.common._
+import net.liftweb.json._
+  import JsonDSL._
+  import Extraction._
+import net.liftweb.util.Helpers._
+
+import dispatch._, Defaults._
+
+case class FeeDetails(
+  amount: Long,
+  currency: String,
+  `type`: String,
+  description: Option[String],
+  application: Option[String]
+)
+
+case class BalanceTransaction(
+  id: String,
+  amount: Long,
+  currency: String,
+  net: Long,
+  `type`: String,
+  created: Long,
+  availableOn: Long,
+  status: String,
+  fee: Long,
+  feeDetails: List[FeeDetails],
+  source: String,
+  description: Option[String],
+  raw: Option[JValue] = None
+) extends StripeObject
+
+object BalanceTransaction extends Gettable[BalanceTransaction] {
+  def baseResourceCalculator(req: Req) = req / "balance" / "history"
+}

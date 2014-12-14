f34754061e1506fa7102299c333b4f7bcda5a1c1
Matt Farmer <matt@frmr.me>
2014-12-14 15:03:43 -0500

    Build out Dispute.

--- src/main/scala/me/frmr/stripe/Dispute.scala
+++ src/main/scala/me/frmr/stripe/Dispute.scala
@@ -4,4 +4,51 @@ import net.liftweb.json._
   import JsonDSL._
 import net.liftweb.util.Helpers._
 
-class Dispute(underlyingData: JValue)
+case class DisputeEvidenceDetails(
+  submissionCount: Int,
+  dueBy: Long
+)
+
+case class DisputeEvidence(
+  productDescription: Option[String],
+  customerName: Option[String],
+  customerEmailAddress: Option[String],
+  customerPurchaseIp: Option[String],
+  customerSignature: Option[String],
+  billingAddress: Option[String],
+  receipt: Option[String],
+  shippingAddress: Option[String],
+  shippingDate: Option[String],
+  shippingTrackingNumber: Option[String],
+  shippingDocumentation: Option[String],
+  refundPolicy: Option[String],
+  refundPolicyDisclosure: Option[String],
+  refundRefusalExplanation: Option[String],
+  cancellationPolicy: Option[String],
+  cancellationPolicyDisclosure: Option[String],
+  cancellationRebuttal: Option[String],
+  accessActivityLog: Option[String],
+  serviceDate: Option[String],
+  serviceDocumentation: Option[String],
+  duplicateChargeId: Option[String],
+  duplicateChargeExplanation: Option[String],
+  duplicateChargeDocumentation: Option[String],
+  customerCommunication: Option[String],
+  uncategorizedText: Option[String],
+  uncategorizedFile: Option[String]
+)
+
+case class Dispute(
+  charge: String,
+  amount: Long,
+  created: Long,
+  currency: String,
+  reason: String,
+  status: String,
+  balanceTransactions: List[BalanceTransaction],
+  evidence: DisputeEvidence,
+  evidenceDetails: DisputeEvidenceDetails,
+  isChargeRefundable: Boolean,
+  metadata: Map[String, String],
+  raw: Option[JValue] = None
+) extends StripeObject

ab1cb64ca494b4d163cb0c8fd70cbc97a02b258b
Mike Rivera <rivera.mj@gmail.com>
2016-03-26 17:51:24 -0400

    new json for card and charge spec

--- src/test/scala/me/frmr/stripe/CardSpec.scala
+++ src/test/scala/me/frmr/stripe/CardSpec.scala
@@ -15,39 +15,42 @@ class CardSpec extends WordSpec with ShouldMatchers {
     "retrieve correct fields from Stripe's JSON" in {
       val exampleCardJson = """
         {
-          "id": "card_14uxPs2eZvKYlo2CzgxT7pbv",
+          "id": "card_17tOG52eZvKYlo2CfEJFhcJ2",
           "object": "card",
-          "last4": "4242",
-          "brand": "Visa",
-          "funding": "credit",
-          "exp_month": 1,
-          "exp_year": 2050,
-          "fingerprint": "Xt5EWLLDS7FJjR1c",
-          "country": "US",
-          "name": null,
-          "address_line1": null,
-          "address_line2": null,
           "address_city": null,
+          "address_country": null,
+          "address_line1": null,
+          "address_line1_check": null,
+          "address_line2": null,
           "address_state": null,
           "address_zip": null,
-          "address_country": null,
-          "cvc_check": "pass",
-          "address_line1_check": null,
           "address_zip_check": null,
+          "brand": "Visa",
+          "country": "US",
+          "customer": null,
+          "cvc_check": "pass",
           "dynamic_last4": null,
-          "customer": null
+          "exp_month": 11,
+          "exp_year": 2022,
+          "fingerprint": "jfYpjs0oYxBYKc4C",
+          "funding": "credit",
+          "last4": "4242",
+          "metadata": {
+          },
+          "name": "tickle.me.edmo@gmail.com",
+          "tokenization_method": null
         }
       """
 
       val testCard = camelifyFieldNames(parse(exampleCardJson)).extract[Card]
 
-      testCard.id should equal("card_14uxPs2eZvKYlo2CzgxT7pbv")
+      testCard.id should equal("card_17tOG52eZvKYlo2CfEJFhcJ2")
       testCard.last4 should equal("4242")
       testCard.brand should equal("Visa")
       testCard.funding should equal("credit")
-      testCard.expMonth should equal(1)
-      testCard.expYear should equal(2050)
-      testCard.fingerprint should equal("Xt5EWLLDS7FJjR1c")
+      testCard.expMonth should equal(11)
+      testCard.expYear should equal(2022)
+      testCard.fingerprint should equal("jfYpjs0oYxBYKc4C")
       testCard.country.get should equal("US")
       testCard.cvcCheck.get should equal("pass")
     }
--- src/test/scala/me/frmr/stripe/ChargeSpec.scala
+++ src/test/scala/me/frmr/stripe/ChargeSpec.scala
@@ -15,83 +15,88 @@ class ChargeSpec extends WordSpec with ShouldMatchers {
     "retrieve correct fields from Stripe's JSON" in {
       val exampleChargeJson = """
         {
-          "id": "ch_14uxPs2eZvKYlo2CIDZ45vfW",
+          "id": "ch_17tNoH2eZvKYlo2CBtLgE7Wa",
           "object": "charge",
-          "created": 1415069492,
-          "livemode": false,
-          "paid": true,
-          "amount": 500,
-          "currency": "usd",
-          "refunded": false,
-          "card": {
-            "id": "card_14uxPs2eZvKYlo2CzgxT7pbv",
-            "object": "card",
-            "last4": "4242",
-            "brand": "Visa",
-            "funding": "credit",
-            "exp_month": 1,
-            "exp_year": 2050,
-            "fingerprint": "Xt5EWLLDS7FJjR1c",
-            "country": "US",
-            "name": null,
-            "address_line1": null,
-            "address_line2": null,
-            "address_city": null,
-            "address_state": null,
-            "address_zip": null,
-            "address_country": null,
-            "cvc_check": "pass",
-            "address_line1_check": null,
-            "address_zip_check": null,
-            "dynamic_last4": null,
-            "customer": null
-          },
-          "captured": true,
-          "refunds": {
-            "object": "list",
-            "total_count": 0,
-            "has_more": false,
-            "url": "/v1/charges/ch_14uxPs2eZvKYlo2CIDZ45vfW/refunds",
-            "data": [
-
-            ]
-          },
-          "balance_transaction": "txn_14smii2eZvKYlo2C2XeEZU0O",
-          "failure_message": null,
-          "failure_code": null,
+          "amount": 300,
           "amount_refunded": 0,
-          "customer": null,
-          "invoice": null,
+          "application_fee": null,
+          "balance_transaction": "txn_17bBwe2eZvKYlo2Cuwcyi9or",
+          "captured": true,
+          "created": 1459023301,
+          "currency": "usd",
+          "customer": "cus_7yobPWbyKiMd1K",
           "description": null,
+          "destination": null,
           "dispute": null,
+          "failure_code": null,
+          "failure_message": null,
+          "fraud_details": {
+          },
+          "invoice": "in_17tMpm2eZvKYlo2CVXsIe10j",
+          "livemode": false,
           "metadata": {
           },
-          "statement_description": null,
-          "fraud_details": {
-            "stripe_report": null,
-            "user_report": null
-          },
+          "order": null,
+          "paid": true,
           "receipt_email": null,
           "receipt_number": null,
-          "shipping": null
+          "refunded": false,
+          "refunds": {
+            "object": "list",
+            "data": [
+
+            ],
+            "has_more": false,
+            "total_count": 0,
+            "url": "/v1/charges/ch_17tNoH2eZvKYlo2CBtLgE7Wa/refunds"
+          },
+          "shipping": null,
+          "source": {
+            "id": "card_17iqyX2eZvKYlo2CTS5vgxM4",
+            "object": "card",
+            "address_city": null,
+            "address_country": null,
+            "address_line1": null,
+            "address_line1_check": null,
+            "address_line2": null,
+            "address_state": null,
+            "address_zip": null,
+            "address_zip_check": null,
+            "brand": "Visa",
+            "country": "US",
+            "customer": "cus_7yobPWbyKiMd1K",
+            "cvc_check": null,
+            "dynamic_last4": null,
+            "exp_month": 12,
+            "exp_year": 2016,
+            "funding": "credit",
+            "last4": "4242",
+            "metadata": {
+            },
+            "name": null,
+            "tokenization_method": null
+          },
+          "source_transfer": null,
+          "statement_descriptor": null,
+          "status": "succeeded"
         }
       """
 
       val testCharge = camelifyFieldNames(parse(exampleChargeJson)).extract[Charge]
 
-      testCharge.id.get should equal("ch_14uxPs2eZvKYlo2CIDZ45vfW")
-      testCharge.created.get should equal(1415069492)
+      testCharge.id.get should equal("ch_17tNoH2eZvKYlo2CBtLgE7Wa")
+      testCharge.created.get should equal(1459023301)
       testCharge.livemode.get should equal(false)
       testCharge.paid.get should equal(true)
-      testCharge.amount.get should equal(500)
+      testCharge.amount.get should equal(300)
       testCharge.currency.get should equal("usd")
       testCharge.refunded.get should equal(false)
       testCharge.captured.get should equal(true)
-      testCharge.balanceTransaction.get should equal("txn_14smii2eZvKYlo2C2XeEZU0O")
+      testCharge.balanceTransaction.get should equal("txn_17bBwe2eZvKYlo2Cuwcyi9or")
       testCharge.failureMessage should equal(None)
       testCharge.failureCode should equal(None)
       testCharge.amountRefunded.get should equal(0)
-      testCharge.customer should equal(None)
+      testCharge.customer should equal(Some("cus_7yobPWbyKiMd1K"))
     }
   }
 }

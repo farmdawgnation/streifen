bc6b04fd8538c2dccbd29144a444f205a747a4d7
Matt Farmer <matt@frmr.me>
2016-04-03 17:39:42 -0400

    Add some scaladocs.

--- src/main/scala/me/frmr/stripe/StripeExecutor.scala
+++ src/main/scala/me/frmr/stripe/StripeExecutor.scala
@@ -48,9 +48,18 @@ class StripeExecutor(
   apiVersion: String = "2016-03-07"
 ) {
   val httpExecutor = new Http()
-  val baseReq = url("https://api.stripe.com/v1").secure.as(apiKey, "") <:< Map("Stripe-Version" -> apiVersion)
+  val baseReq = url("https://api.stripe.com/v1").secure.as(apiKey, "") <:<
+    Map("Stripe-Version" -> apiVersion, "User-Agent" -> "streifen")
   implicit val formats = DefaultFormats
 
+  /**
+   * Execute a request against Stripe and get a {{Full(StripeResponse)}} in reponse if things work
+   * (e.g. return a 200) and a {{Failure}} otherwise.
+   *
+   * In general, the primary consumer of this function will be the streifen library itself. External
+   * code shouldn't need to use this unless it's implementing functionality not supported by
+   * streifen for some reason.
+  **/
   def execute(request: Req): Future[Box[StripeResponse]] = {
     httpExecutor(request > AsStripeResponse).either.map {
       case Left(throwable) =>
@@ -69,6 +78,14 @@ class StripeExecutor(
       Failure(s"Unexpected $code") ~> json
   }
 
+  /**
+   * Execute a request against Stripe and get a fully typed object out of the response if it was
+   * successful.
+   *
+   * In general, the primary consumer of this function will be the streifen library itself. External
+   * code shouldn't need to use this unless it's implementing functionality not supported by
+   * streifen for some reason.
+  **/
   def executeFor[T <: StripeObject](request: Req)(implicit mf: Manifest[T]): Future[Box[T]] = {
     execute(request).map { futureBox =>
       for {
